68657e0e0acff1c5d803989a6416feb5def9c124
Rafał Sochacki <sochacki.rfl@gmail.com>
2022-03-28 23:15:33 +0200

    MotionBridge initial commit

---
+++ src/main/java/com/motionbridge/motionbridge/MotionbridgeApplication.java
@@ -0,0 +1,13 @@
+package com.motionbridge.motionbridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotionbridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotionbridgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/motionbridge/motionbridge/MotionbridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motionbridge.motionbridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotionbridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
