a884adf57bd4ed512ead1c7993447e8e195b039f
Rafał Sochacki <sochacki.rfl@gmail.com>
2022-08-27 14:55:42 +0100

    commit fail

--- src/main/java/com/motionbridge/motionbridge/Finder/commons/port/ChromeDriverUseCase.java
+++
@@ -1,8 +0,0 @@
-package com.motionbridge.motionbridge.Finder.commons.port;
-
-import org.openqa.selenium.WebDriver;
-
-public interface ChromeDriverUseCase {
-    WebDriver initializeChromeDriver();
-    void shutDownDriver(WebDriver driver);
-}
